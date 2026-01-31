565f6a262ee9faa7a3986fd2a7df04c8e8aaaa9d
givecoffee <giveitcoffee@gmail.com>
2026-01-31 05:03:56 -0800

    Ready for merge with main branch + added new tooling

--- node-server/eslint.config.js
+++ node-server/eslint.config.js
@@ -1,7 +1,19 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import globals from 'globals';
+import js from '@eslint/js';
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        ...globals.node
+      }
+    },
+    rules: {
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single']
+    }
+  }
+];
