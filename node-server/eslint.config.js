cff7cfaf569d30221061c54cb45d2c14967c5136
givecoffee <giveitcoffee@gmail.com>
2026-01-31 05:39:09 -0800

    fix: add ESLint dependencies and fix linting errors

--- node-server/eslint.config.js
+++ node-server/eslint.config.js
@@ -1,19 +1,19 @@
-import globals from "globals";
-import js from "@eslint/js";
+import globals from 'globals';
+import js from '@eslint/js';
 
 export default [
   js.configs.recommended,
   {
     languageOptions: {
       ecmaVersion: 2025,
-      sourceType: "module",
+      sourceType: 'module',
       globals: {
         ...globals.node
       }
     },
     rules: {
-      "semi": ["error", "always"],
-      "quotes": ["error", "single"]
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single']
     }
   }
 ];
