a90a82f9121d6e1af73f36f890fc13ac67166828
givecoffee <rachel.maffei@seattlecolleges.edu>
2026-03-09 23:58:38 -0700

    Update ECMAScript version in ESLint config

--- node-server/eslint.config.js
+++ node-server/eslint.config.js
@@ -5,11 +5,7 @@ export default [
   js.configs.recommended,
   {
     languageOptions: {
-<<<<<<< HEAD
-      ecmaVersion: 2025,
-=======
-      ecmaVersion: 2021,
->>>>>>> 06162fb3f2b1f684b2ecea8891768b4da898137d
+      ecmaVersion: 2025,    // ✅ Pick whichever version you want
       sourceType: 'module',
       globals: {
         ...globals.node
